c7b18849a727142021a1acd422e30bb60fe71dfa
Vittorio00 <viktoryturiaci2323@gmail.com>
2024-11-19 12:14:47 +0100

    Formatted documents

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,17 +1,15 @@
 const btnNext = document.getElementById('proceed');
-const checkbox=document.getElementById('checkbox');
+const checkbox = document.getElementById('checkbox');
 
 btnNext.addEventListener('click', proceed);
 
-checkbox.addEventListener('change',() => {
-    btnNext.disabled=!checkbox.checked
+checkbox.addEventListener('change', () => {
+  btnNext.disabled = !checkbox.checked;
 });
 
-function proceed(e){
-    e.preventDefault();
-    if (checkbox.checked) {
-    window.location.href='index-2.html';
-    }
+function proceed(e) {
+  e.preventDefault();
+  if (checkbox.checked) {
+    window.location.href = 'index-2.html';
+  }
 }
-
-
