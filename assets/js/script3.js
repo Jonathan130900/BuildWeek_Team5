ede01c1388c338927ab79b1c3cf7461840b14f6f
Vittorio00 <viktoryturiaci2323@gmail.com>
2024-11-21 09:11:54 +0100

    Modifiche lato CSS: traccia nel recap delle domande spostato al centro

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,16 +1,16 @@
-const results = document.getElementById("results");
-const btnRate = document.getElementById("btnRate");
-const correct = document.getElementById("percentageCorrect");
-const wrong = document.getElementById("percentageWrong");
-const answersList = document.getElementById("answerRecap");
-const chartText = document.getElementById("chartText");
+const results = document.getElementById('results');
+const btnRate = document.getElementById('btnRate');
+const correct = document.getElementById('percentageCorrect');
+const wrong = document.getElementById('percentageWrong');
+const answersList = document.getElementById('answerRecap');
+const chartText = document.getElementById('chartText');
 
 const punteggioFinale = ottieniPunteggio();
 const punteggioTotale = 10;
 const rightPercentage = (punteggioFinale / punteggioTotale) * 100;
 const wrongPercentage = 100 - rightPercentage;
 
-document.addEventListener("load", init());
+document.addEventListener('load', init());
 
 function init() {
   flowchart(rightPercentage, wrongPercentage);
@@ -21,7 +21,7 @@ function init() {
 }
 
 function ottieniPunteggio() {
-  const data = JSON.parse(localStorage.getItem("quizResults"));
+  const data = JSON.parse(localStorage.getItem('quizResults'));
   return data.score;
 }
 
@@ -61,7 +61,7 @@ function launchConfetti() {
 // Effetto sonoro fail test
 function playSadMusic() {
   console.log(":'(");
-  const audio = new Audio("assets/audio/violino_triste_piango_forte.mp3");
+  const audio = new Audio('assets/audio/violino_triste_piango_forte.mp3');
   audio.play();
   audio.volume = 0.5;
   audio.loop = false;
@@ -69,20 +69,20 @@ function playSadMusic() {
 
 // Effetto sonoro success test
 function cheersGoodJob() {
-  console.log(":D");
-  const audio = new Audio("assets/audio/ma_quanto_sei_forte_complimenti.mp3");
+  console.log(':D');
+  const audio = new Audio('assets/audio/ma_quanto_sei_forte_complimenti.mp3');
   audio.play();
   audio.volume = 0.5;
   audio.loop = false;
 }
 
 function ottieniDomande() {
-  const data = JSON.parse(localStorage.getItem("quizResults")) || {
+  const data = JSON.parse(localStorage.getItem('quizResults')) || {
     score: 0,
     answers: [],
   };
 
-  const risultatoContainer = document.getElementById("answersList");
+  const risultatoContainer = document.getElementById('answersList');
   risultatoContainer.innerHTML = `
     <h2>Quiz completed!</h2>
     <p>You obtained ${data.score}/${punteggioTotale} points.</p>    
@@ -97,33 +97,34 @@ function ottieniDomande() {
     playSadMusic();
   }
 
-  risultatoContainer.style.marginTop = "100px";
-  const listaRisposte = document.createElement("ul");
+  risultatoContainer.style.marginTop = '100px';
+  const listaRisposte = document.createElement('ul');
   listaRisposte.innerHTML = `
     <p>Questions details:</p>`;
-  listaRisposte.style.listStyle = "none";
+  listaRisposte.style.listStyle = 'none';
   data.answers.forEach((risposta, index) => {
-    const listItem = document.createElement("li");
+    const listItem = document.createElement('li');
 
     // Aggiungere la domanda e le risposte
     listItem.innerHTML = `
-      <p id = 'numeroDomanda'>Question ${index + 1}:</p> ${
+      <p id = 'numeroDomanda'>Question ${index + 1}:</p> <p>${
       risposta.question
-    }<br>
+    } </p>
+    <br>
       <strong>Given answer:</strong> ${risposta.givenAnswer} <br>
       <strong>Right answer:</strong> ${risposta.correctAnswer} <br>
       <strong>result:</strong> <strong id="result-${index}">${
-      risposta.isCorrect ? "Correct" : "Wrong"
+      risposta.isCorrect ? 'Correct' : 'Wrong'
     }</strong>
     `;
 
-    listItem.style.textAlign = "left";
-    listItem.style.padding = "20px";
-    listaRisposte.style.border = "2px solid purple";
+    listItem.style.textAlign = 'left';
+    listItem.style.padding = '20px';
+    listaRisposte.style.border = '2px solid purple';
 
     // Imposta il colore solo per il risultato
     const resultElement = listItem.querySelector(`#result-${index}`);
-    resultElement.style.color = risposta.isCorrect ? "green" : "red";
+    resultElement.style.color = risposta.isCorrect ? 'green' : 'red';
 
     listaRisposte.appendChild(listItem);
   });
@@ -132,25 +133,25 @@ function ottieniDomande() {
 }
 
 function flowchart(result, difference) {
-  const ctx = document.getElementById("myFlowChart").getContext("2d");
+  const ctx = document.getElementById('myFlowChart').getContext('2d');
 
   const data = {
-    labels: ["Wrong Answers", "Correct Answers"],
+    labels: ['Wrong Answers', 'Correct Answers'],
     datasets: [
       {
-        label: "Risultati",
+        label: 'Risultati',
         data: [difference, result],
-        backgroundColor: ["#C2128D", "#00FFFF"],
-        borderColor: ["#C2128D", "#00FFFF"],
+        backgroundColor: ['#C2128D', '#00FFFF'],
+        borderColor: ['#C2128D', '#00FFFF'],
       },
     ],
   };
 
   const config = {
-    type: "doughnut",
+    type: 'doughnut',
     data: data,
     options: {
-      cutout: "75%",
+      cutout: '75%',
       responsive: false,
       plugins: {
         legend: {
@@ -165,8 +166,8 @@ function flowchart(result, difference) {
 }
 
 function resultText() {
-  const newH4 = document.createElement("h4");
-  const newP = document.createElement("p");
+  const newH4 = document.createElement('h4');
+  const newP = document.createElement('p');
 
   // Controlla il punteggio e determina il messaggio da mostrare
   if (punteggioFinale > punteggioTotale / 2) {
@@ -181,20 +182,20 @@ function resultText() {
   }
 
   // Stile aggiuntivo per il testo (opzionale)
-  newH4.style.textAlign = "center";
-  newH4.style.color = punteggioFinale > punteggioTotale / 2 ? "green" : "red";
-  newP.style.textAlign = "center";
+  newH4.style.textAlign = 'center';
+  newH4.style.color = punteggioFinale > punteggioTotale / 2 ? 'green' : 'red';
+  newP.style.textAlign = 'center';
 }
 
-btnRate.addEventListener("click", function () {
-  window.location.href = "index-4.html";
+btnRate.addEventListener('click', function () {
+  window.location.href = 'index-4.html';
   resetLocalStorage();
 });
 
 //Funzione per mostrare a schermo la percentuale giusta
 function correctAnswer() {
-  const newP = document.createElement("p");
-  newP.id = "correctP";
+  const newP = document.createElement('p');
+  newP.id = 'correctP';
   const correctPercentage = `${rightPercentage}%`;
   newP.innerText = correctPercentage;
   correct.appendChild(newP);
@@ -202,8 +203,8 @@ function correctAnswer() {
 
 //Funzione per mostrare a schermo la percentuale sbagliata
 function wrongAnswer() {
-  const newP = document.createElement("p");
-  newP.id = "wrongP";
+  const newP = document.createElement('p');
+  newP.id = 'wrongP';
   const percentageWrong = `${wrongPercentage}%`;
   newP.innerText = percentageWrong;
   wrong.appendChild(newP);
@@ -212,7 +213,7 @@ function wrongAnswer() {
 //Funzione per resettare il localstorage
 function resetLocalStorage() {
   localStorage.setItem(
-    "quizResults",
+    'quizResults',
     JSON.stringify({
       score: 0,
       answers: [],
